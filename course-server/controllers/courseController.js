acad5fd3ee0d4d8a292891438af8c76e65f87ac6
rinku-kr <rk24736599@gmail.com>
2024-03-03 23:24:48 +0530

    Creating first api

--- course-server/controllers/courseController.js
+++ course-server/controllers/courseController.js
@@ -1,3 +1,9 @@
-export const getAllCourses = (req, res, next) => {
-  res.send("hello world");
+import { Course } from "../models/courseModel.js";
+
+export const getAllCourses = async (req, res, next) => {
+  const courses = await Course.find();
+  res.status(200).json({
+    success: true,
+    courses,
+  });
 };
